d273147f7d8ffd4c1e0d552151f8b076e23ab6d9
arne <arne.ziegler@gmx.net>
2024-07-09 20:07:55 +0000

    Update Makefile

--- c2/hello-tail.py
+++ c2/hello-tail.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from bcc import BPF
+import ctypes as ct
 
 program = r"""
 BPF_PROG_ARRAY(syscall, 300);
@@ -18,15 +19,19 @@ int hello_execve(void *ctx) {
 }
 
 int hello_timer(struct bpf_raw_tracepoint_args *ctx){
-  if (ctx->args[1] == 222 {
-      bpf_trace_printk("Creating a timer");
-  } else if (ctx->args[1] == 226) {
-      bpf_trace_printk("Deleting a time");
-  } else {
-      bpf_trace_printk("Some other timer operation");
+  int opcode = ctx->args[1];
+  switch (opcode) {
+      case 222:
+          bpf_trace_printk("Creating a timer");
+          break;
+      case 226:
+          bpf_trace_printk("Deleting a time");
+          break;
+      default:
+          bpf_trace_printk("Some other timer operation");
+          break;
   }
   return 0;
-
 }
 
 int ignore_opcode(void *ctx) {
@@ -35,16 +40,22 @@ int ignore_opcode(void *ctx) {
 """
 
 b = BPF(text=program)
-syscall = b.get_syscall_fnname("execve")
-b.attach_kprobe(event=syscall, fn_name="hello")
+b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
+ignore_fn = b.load_func("ignore_opcode", BPF.RAW_TRACEPOINT)
+exec_fn = b.load_func("hello_execve", BPF.RAW_TRACEPOINT)
+timer_fn = b.load_func("hello_timer", BPF.RAW_TRACEPOINT)
 
+prog_array = b.get_table("syscall")
+prog_array[ct.c_int(59)] = ct.c_int(exec_fn.fd)
+prog_array[ct.c_int(222)] = ct.c_int(timer_fn.fd)
+prog_array[ct.c_int(223)] = ct.c_int(timer_fn.fd)
+prog_array[ct.c_int(224)] = ct.c_int(timer_fn.fd)
+prog_array[ct.c_int(225)] = ct.c_int(timer_fn.fd)
+prog_array[ct.c_int(226)] = ct.c_int(timer_fn.fd)
 
-def print_event(cpu, data, size):
-    data = b["output"].event(data)
-    print(f"{data.pid} {data.uid} {data.command.decode()} " +
-          f"{data.message.decode()}")
+# ignore some syscalls that come up a lot
+prog_array[ct.c_int(21)] = ct.c_int(ignore_fn.fd)
+prog_array[ct.c_int(22)] = ct.c_int(ignore_fn.fd)
+prog_array[ct.c_int(25)] = ct.c_int(ignore_fn.fd)
 
-
-b["output"].open_perf_buffer(print_event)
-while True:
-    b.perf_buffer_poll()
+b.trace_print()
--- c3/hello.bpf.c
+++ c3/hello.bpf.c
@@ -1,3 +1,4 @@
+#include <linux/types.h> 
 #include <bpf/bpf_helpers.h>
 #include <linux/bpf.h>
 
